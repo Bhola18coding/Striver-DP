5a0c91596bdb0541359c38fbbb77a5d7898f5a46
Bhola18coding <patranirmal243@gmail.com>
2024-01-13 12:14:52 +0530

    done

--- Lec01-Nth_Fibonacci.cpp
+++ Lec01-Nth_Fibonacci.cpp
@@ -1,3 +1,7 @@
+//MEMOIZATION 
+// TC =O(N)
+// SC =O(N)- Recursive Stack Space , O(N) = Dp array
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -32,4 +36,32 @@ int main()
        vector<int>dp(n+1, -1);
        cout<<fib(n, dp);
        return 0;
+}
+
+
+
+
+
+///Tabulation = Dp  
+// TC =O(N), O(1)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int n;
+   cin>>n; 
+   int pre2 = 0;
+   int pre1 = 1;
+   for(int i=2; i<=n; i++)
+   {
+     int curri = pre2 + pre1;
+
+      pre2 = pre1;
+      pre1 = curri;
+     
+   }
+
+   cout<<pre1;
 }
